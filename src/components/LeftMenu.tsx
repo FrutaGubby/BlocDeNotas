c0b683a0af83e983ea38d0a7fc0b807d72f53995
FrutaGubby <ronaldo-equi@hotmail.com>
2023-06-11 10:33:43 -0700

    Creacion de proyecto

---
+++ src/components/LeftMenu.tsx
@@ -0,0 +1,13 @@
+export default function Menu() {
+  return (
+    <>
+        <div className="container">
+            
+                <p>3</p>
+                <p>2</p>
+                <p>1</p>
+            
+        </div>
+    </>
+  )
+}
